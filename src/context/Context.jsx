7877fe981c614afa1d6c8d115cc9c9bbd88998be
SeungJin051 <seungjin051@gmail.com>
2024-05-11 16:19:32 +0900

    답변 애니메이션 추가

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -25,7 +25,11 @@ const ContextProvider = (props) => {
   const [resultData, setResultData] = useState("");
 
   // 타이핑시 효과 로직 (입력 필드에 대한 AI 값을 받은 데이터 텍스트)
-  const delayPara = (index, nextWord) => {};
+  const delayPara = (index, nextWord) => {
+    setTimeout(function () {
+      setResultData((prev) => prev + nextWord);
+    }, 75 * index);
+  };
 
   // API 통신 로직
   const onSend = async (prompt) => {
@@ -39,8 +43,8 @@ const ContextProvider = (props) => {
 
     // ** 제거
     let resArray = response.split("**");
-    let newRes;
-    // ** 제거
+    let newRes = "";
+    // ** 제거 i가 0이거나 홀수인지를 확인
     for (let i = 0; i < resArray.length; i++) {
       if (i === 0 || i % 2 !== 1) {
         newRes += resArray[i];
@@ -50,7 +54,31 @@ const ContextProvider = (props) => {
       }
     }
 
-    setResultData(newRes);
+    // ** 제거 i가 0이거나 홀수인지를 확인
+    for (let i = 0; i < resArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newRes += resArray[i];
+      } else {
+        // ** -> bold 처리
+        newRes += "<b>" + resArray[i] + "</b>";
+      }
+    }
+    // 링크를 a 태그로 감싸서 href 처리
+    newRes = newRes.replace(
+      /(https?:\/\/[^\s]+)/g,
+      '<a href="$1" target="_blank">$1</a>'
+    );
+
+    // 문자열을 배열로 분할하여 각각의 단어를 적절한 딜레이와 함께 표시
+    let newRes2 = newRes.split("*").join("</br>");
+
+    // ## 정규 표ㅕ현식으로 제거
+    newRes2 = newRes2.replace(/##/g);
+    let newResArray = newRes2.split(" ");
+    for (let i = 0; i < newResArray.length; i++) {
+      const nextWord = newResArray[i];
+      delayPara(i, nextWord + " ");
+    }
     setLoading(false);
     setInput("");
   };
